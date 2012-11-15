635223a5b73d0064c6ac64b932bfc807cdbd08e3
jjankovi <jjankovi@redhat.com>
2012-11-15 12:37:12 +0100

    Added redrawUI method to react to cache state

--- src/org/infinispan/android/app/MainActivity.java
+++ src/org/infinispan/android/app/MainActivity.java
@@ -8,12 +8,24 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private LocalCacheManager localCache;
+	
+	private boolean cacheStarted;
+	
+	private Button startButton;
+	private Button stopButton;
+	private Button valuesButton;
+	private Button putButton;
+	private Button removeButton;
+	
+	private EditText key;
+	private EditText value;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,8 @@ public class MainActivity extends Activity {
 		Thread.currentThread().setContextClassLoader(
 				getClass().getClassLoader());
 		localCache = new LocalCacheManager();
+		cacheStarted = false;
+		redrawUI();
 	}
 
 	@Override
@@ -33,16 +47,16 @@ public class MainActivity extends Activity {
 
 	/** Called when the start button is pressed */
 	public void start(View view) {
-
+		
 		new StartCacheTask().execute();
-
+		
 	}
 
 	/** Called when the stop button is pressed */
 	public void stop(View view) {
 
 		new StopCacheTask().execute();
-
+		
 	}
 	
 	/** Called when the put is pressed */
@@ -91,12 +105,37 @@ public class MainActivity extends Activity {
 		
 	}
 	
+	private void redrawUI() {
+		
+		key = (EditText)findViewById(R.id.editText1);
+		key.setEnabled(cacheStarted);
+		value = (EditText)findViewById(R.id.editText2);
+		value.setEnabled(cacheStarted);
+		startButton = (Button)findViewById(R.id.button1);
+		startButton.setEnabled(!cacheStarted);
+		stopButton = (Button)findViewById(R.id.button2);
+		stopButton.setEnabled(cacheStarted);
+		putButton = (Button)findViewById(R.id.button3);
+		putButton.setEnabled(cacheStarted);
+		valuesButton = (Button)findViewById(R.id.button4);
+		valuesButton.setEnabled(cacheStarted);
+		removeButton = (Button)findViewById(R.id.button5);
+		removeButton.setEnabled(cacheStarted);
+		
+	}
+	
 	private class StartCacheTask extends AsyncTask<Void, Void, Void> {
 
 		@Override
 		protected Void doInBackground(Void... params) {
 			localCache.startCache();
+			cacheStarted = true;
 			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			redrawUI();
 		}
 		
 	}
@@ -106,7 +145,13 @@ public class MainActivity extends Activity {
 		@Override
 		protected Void doInBackground(Void... params) {
 			localCache.stopCache();
+			cacheStarted = false;
 			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			redrawUI();
 		}
 		
 	}
